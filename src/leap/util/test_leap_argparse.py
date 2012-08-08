0ac0cbb9f6dd91a414747f2a59d5a9d1bbfee571
kali <kali@leap.se>
2012-08-08 19:36:17 +0900

    stub test for leap_argparse

---
+++ src/leap/util/test_leap_argparse.py
@@ -0,0 +1,27 @@
+from argparse import Namespace
+import unittest
+
+from leap.util import leap_argparse
+
+
+class LeapArgParseTest(unittest.TestCase):
+    """
+    Test argparse options for eip client
+    """
+
+    def setUp(self):
+        """
+        get the parser
+        """
+        self.parser = leap_argparse.build_parser()
+
+    def test_debug_mode(self):
+        """
+        test debug mode option
+        """
+        opts = self.parser.parse_args(
+            ['--debug'])
+        self.assertEqual(
+            opts,
+            Namespace(config_file=None,
+                      debug=True))
