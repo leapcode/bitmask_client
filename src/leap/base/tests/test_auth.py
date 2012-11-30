e7dbf89f31711271e61f653e1cc7fb2c2b57cc6e
antialias <antialias@leap.se>
2012-11-30 18:04:13 -0500

    to improve code coverage, began writing tests for leap.base.auth.

---
+++ src/leap/base/tests/test_auth.py
@@ -0,0 +1,57 @@
+from BaseHTTPServer import BaseHTTPRequestHandler
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+import requests
+from mock import Mock
+
+from leap.base import auth
+from leap.base import exceptions
+from leap.eip.tests.test_checks import NoLogRequestHandler
+from leap.testing.basetest import BaseLeapTest
+from leap.testing.https_server import BaseHTTPSServerTestCase
+
+
+class LeapSRPRegisterTests(BaseHTTPSServerTestCase, BaseLeapTest):
+    __name__ = "leap_srp_register_test"
+    provider = "testprovider.example.org"
+
+    class request_handler(NoLogRequestHandler, BaseHTTPRequestHandler):
+        responses = {
+            '/': ['OK', ''],
+                }
+
+        def do_GET(self):
+            path = urlparse.urlparse(self.path)
+            message = '\n'.join(self.responses.get(
+                path.path, None))
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(message)
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_srp_auth_should_implement_check_methods(self):
+        SERVER = "https://localhost:8443"
+        srp_auth = auth.LeapSRPRegister(provider=SERVER, verify=False)
+
+        self.assertTrue(hasattr(srp_auth, "init_session"),
+                        "missing meth")
+        self.assertTrue(hasattr(srp_auth, "get_registration_uri"),
+                        "missing meth")
+        self.assertTrue(hasattr(srp_auth, "register_user"),
+                        "missing meth")
+
+    def test_srp_auth_basic_functionality(self):
+        SERVER = "https://localhost:8443"
+        srp_auth = auth.LeapSRPRegister(provider=SERVER, verify=False)
+
+        self.assertIsInstance(srp_auth.session, requests.sessions.Session)
+        self.assertEqual(srp_auth.get_registration_uri(),
+                "https://localhost:8443/1/users.json")
