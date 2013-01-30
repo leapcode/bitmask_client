564c776fe22ae8451aaec9b153261da8744a2770
kali <kali@leap.se>
2013-01-30 12:06:59 +0900

    preliminary watch

--- debian/watch.ex
+++ debian/watch.ex
@@ -6,18 +6,5 @@
 # Compulsory line, this is a version 3 file
 version=3
 
-# Uncomment to examine a Webpage
-# <Webpage URL> <string match>
-#http://www.example.com/downloads.php python-leap-client-(.*)\.tar\.gz
-
-# Uncomment to examine a Webserver directory
-#http://www.example.com/pub/python-leap-client-(.*)\.tar\.gz
-
-# Uncommment to examine a FTP server
-#ftp://ftp.example.com/pub/python-leap-client-(.*)\.tar\.gz debian uupdate
-
-# Uncomment to find new files on sourceforge, for devscripts >= 2.9
-# http://sf.net/python-leap-client/python-leap-client-(.*)\.tar\.gz
-
-# Uncomment to find new files on GooglePages
-# http://example.googlepages.com/foo.html python-leap-client-(.*)\.tar\.gz
+# XXX Leaving this here until we have leap.se downloads page -- kali
+https://github.com/leapcode/leap_client/tags.*/v(\d[\d\.]+)\.tar\.gz
