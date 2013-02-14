5151bb97b175505a8ebf9d3e3ba31454f7ebb159
kali <kali@leap.se>
2013-02-15 07:46:11 +0900

    fix polkit auth detection

--- src/leap/util/polkit.py
+++ src/leap/util/polkit.py
@@ -1,13 +1,14 @@
 import logging
 
 import sh
-from sh import grep
-from sh import ps
 
 logger = logging.getLogger(__name__)
 
 
 def run_polkit_auth_agent():
+    """
+    launches polkit authentication agent
+    """
     logger.debug('launching policykit authentication agent in background...')
     polkit = sh.Command('/usr/lib/policykit-1-gnome/'
                         'polkit-gnome-authentication-agent-1')
@@ -19,8 +20,17 @@ def check_if_running_polkit_auth():
     check if polkit authentication agent is running
     and launch it if it is not
     """
+    from sh import grep
+    from sh import ps
+
+    # for some reason, sh is getting us
+    # limited line width, so the grep fails
+    #grep_pk = lambda: grep(ps('a'), 'polkit-gnome-authentication-agent-1')
+    grep_pk = lambda: grep(ps('a'), '[p]olkit-gnome-auth')
     try:
-        grep(ps('aux'), '[p]olkit-gnome-authentication-agent-1')
+        grep_pk()
     except sh.ErrorReturnCode_1:
         logger.debug('polkit auth agent not found, trying to launch it...')
         run_polkit_auth_agent()
+    else:
+        logger.debug('polkit auth agent is running, no need to be launched')
