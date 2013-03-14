1f4841079a72df874d0624f7908115ded295a82f
kali <kali@leap.se>
2013-03-15 01:13:30 +0900

    remove branding remnant, add missing header

--- src/leap/util/__init__.py
+++ src/leap/util/__init__.py
@@ -1,6 +1,21 @@
+# -*- coding: utf-8 -*-
+# __init__.py
+# Copyright (C) 2013 LEAP
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-LEAP Encryption Access Project
-website: U{https://leap.se/}
+Initializes version and app info
 """
 
 __version__ = "unknown"
@@ -22,8 +37,3 @@ except ImportError:
     pass
 
 __full_version__ = __appname__ + '/' + str(__version__)
-
-# try:
-#     from leap._branding import BRANDING as __branding
-# except ImportError:
-#     __branding = {}
