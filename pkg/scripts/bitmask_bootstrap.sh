5445d5868236b1de481f44f7c4314d93b19f2898
Kali Kaneko <kali@leap.se>
2013-10-16 01:11:27 -0300

    add make step to bootstrap script

--- pkg/scripts/bitmask_bootstrap.sh
+++ pkg/scripts/bitmask_bootstrap.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
-
+######################################################################
+# bitmask_boostrap.sh
+# Copyright (C) 2013 LEAP
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+######################################################################
 # Installs requirements, and
 # clones the latest leap-client
 
 # depends on:
-# openvpn git-core libgnutls-dev python-dev python-qt4 python-setuptools python-virtualenv
+# (authoritative list under docs/dev/quickstart.rst)
+
+# git python-dev python-setuptools python-virtualenv python-pip python-openssl libsqlite3-dev g++ openvpn
+# pyside-tools python-pyside python-qt4
+
+# Clone latest git (develop branch)
+# change "develop" for any other branch you want.
+BRANCH="develop"
+BITMASK_DIR="bitmask-develop"
 
 # Escape code
 esc=`echo -en "\033"`
@@ -23,27 +47,30 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~"
 echo ""
 echo "${cc_green}Creating virtualenv...${cc_normal}"
 
-mkdir bitmask-testbuild
-virtualenv bitmask-testbuild
-source bitmask-testbuild/bin/activate
+mkdir ${BITMASK_DIR}
+virtualenv "${BITMASK_DIR}"
+source ./${BITMASK_DIR}/bin/activate
 
 echo "${cc_green}Installing bitmask...${cc_normal}"
 
-# Clone latest git (develop branch)
-# change "develop" for any other branch you want.
+pip install -e 'git+https://leap.se/git/bitmask_client@'${BRANCH}'#egg=leap.bitmask'
 
-
-pip install -e 'git+https://leap.se/git/bitmask_client@develop#egg=leap.bitmask'
-
-cd bitmask-testbuild
+cd ${BITMASK_DIR}
 
 # symlink the pyside libraries to the system libs
 ./src/leap.bitmask/pkg/postmkvenv.sh
+
+cd ./src/leap.bitmask
+make
+cd ../../
+source ./bin/activate
 
 echo "${cc_green}bitmask installed! =)"
 echo "${cc_yellow}"
 echo "Launch it with: "
 echo "~~~~~~~~~~~~~~~~~~~~~~"
-echo "bin/bitmask"
+echo "bin/bitmask --debug"
 echo "~~~~~~~~~~~~~~~~~~~~~~"
+echo "If you are not inside the virtualenv, source it first with "
+echo "source "${BITMASK_DIR}"/bin/activate"
 echo "${cc_normal}"
