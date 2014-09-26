0db3d5a4aecc0a3c271b92c6187922c50d46df0a
Tomás Touceda <chiiph@leap.se>
2014-09-26 10:13:44 -0300

    Revert "Fix soledad imports (#5989)."
    This reverts commit 7fcc4f40eaa8214de8ae20cd71d173337ad64290.

--- src/leap/bitmask/services/soledad/soledadbootstrapper.py
+++ src/leap/bitmask/services/soledad/soledadbootstrapper.py
@@ -45,8 +45,7 @@ from leap.common.files import which
 from leap.keymanager import KeyManager, openpgp
 from leap.keymanager.errors import KeyNotFound
 from leap.soledad.common.errors import InvalidAuthTokenError
-from leap.soledad.client import Soledad
-from leap.soledad.client.secrets import BootstrapSequenceError
+from leap.soledad.client import Soledad, BootstrapSequenceError
 
 logger = logging.getLogger(__name__)
 
