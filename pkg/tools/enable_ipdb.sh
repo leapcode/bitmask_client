43a67207845943d5e93c51844feb6666ac1911f5
Kali Kaneko <kali@leap.se>
2014-11-25 12:04:31 +0100

    fix typo, add license

--- pkg/tools/enable_ipdb.sh
+++ pkg/tools/enable_ipdb.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
-# This script installs modules needed for using IPythin debug shell in a
+# enable_ipdb.sh
+# This script installs modules needed for using IPython debug shell in a
 # Bitmask bundle directory. It uses a python virtual environment in which it
 # installs needed modules and then links them into the appropriate directory
 # inside the bundle directory.
+
+# Copyright (C) 2013 LEAP
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 MODULES="ast.py runpy.py"
 SITE_MODULES="ipdb IPython simplegeneric.py decorator.py pexpect"
