dde1b5289b12fa034eae10dfdf9354d0c7cd93bc
Kali Kaneko <kali@leap.se>
2015-08-24 17:42:20 -0400

    [pkg] fetch origin before checkout

--- pkg/scripts/checkout_leap_versions.sh
+++ pkg/scripts/checkout_leap_versions.sh
@@ -3,5 +3,5 @@ cat pkg/leap_versions.txt | while read line
 do
 	package=$(echo $line | cut -f1 -d' ')
 	tag=$(echo $line | cut -f2 -d' ')
-	cd ../$package && git checkout $tag
+	cd ../$package && git fetch origin && git checkout $tag
 done
