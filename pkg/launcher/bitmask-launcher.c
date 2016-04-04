093f84e42192fd8dc2234a00b754515be7ea9016
Kali Kaneko <kali@leap.se>
2016-04-04 17:51:30 -0400

    [pkg] simple launcher for the linux bundles

---
+++ pkg/launcher/bitmask-launcher.c
@@ -0,0 +1,23 @@
+/*
+ * bitmask-launcher.c
+ *
+ * part of the bitmask bundle.
+ * execute main entrypoint in a child folder inside the bundle.
+ *
+ * (c) LEAP Encryption Access Project, 2016.
+ * License: GPL.
+ *
+*/
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char* const bitmask_path = "libs";
+char* const entrypoint = "bitmask";
+
+int main(int argc, char *argv[])
+{
+    argv[0] = entrypoint;
+    chdir(bitmask_path);
+    execv(entrypoint, argv);
+}
